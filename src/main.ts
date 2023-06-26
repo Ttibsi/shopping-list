1f3614165d8bc9ec08534f3e297f911c6592f9bc
Ttibsi <ashisbitt@icloud.com>
2023-06-26 16:59:57 +0100

    Setup livereloading, update ts for adding classes

--- src/main.ts
+++ src/main.ts
@@ -15,12 +15,23 @@ if (app) {
 `
 }
 
-let val = fetch("http://localhost:8888/getEntries").then(response => response.json()).then(vals => {
+fetch("http://localhost:8888/getEntries").then(response => response.json()).then(vals => {
     const list = document.getElementById("list") as HTMLUListElement
     if (list) {
         vals.values.forEach((obj: ShoppingItem) => {
             // console.log("entry is " + obj.id)
-            list.innerHTML += `<li>${obj.value}</li>`
+            if (obj.completed) {
+                list.innerHTML += `<li class="cmpltd"><button class="cmpltd_btn"></button>${obj.value}</li>`
+            } else {
+                list.innerHTML += `<li><button></button>${obj.value}</li>`
+            }
         })
     }
 })
+
+// Instead of bullet points, lets do this as a button so we can change
+// how it's displayed. In the CSS we remove the li bullet points, and here we
+// prepend the text with a button and style it in CSS. If obj.completed === True
+// we then add a class to the html button to change how it looks if the item
+// is complete. Can we also update the css of the text if the button has is true
+// or should we just also give it a class in the JS amending
